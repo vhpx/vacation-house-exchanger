29475d0c531ca2b48436b221b3a812eeb3110b07
toulletou03 <104828438+toulletou03@users.noreply.github.com>
2022-05-09 16:53:35 +0700

    classes building

--- class.cpp
+++ class.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 #include <vector>
+
+//* Helper preprocessor macros
+// Miscellaneous macros
+#define newl "\n"     // skip 1 line
+#define new2l "\n\n"  // skip 2 lines
+
+// I/O macros
+#define illog(x) std::cout << x                        // Inline print
+#define log(x) std::cout << x << newl                  // Print with newline
+#define debug(x) std::cout << #x << ": " << x << newl  // Print with variable name
+#define show_error(x) std::cerr << x << newl           // Print error
+#define input(x) std::cin >> x                         // Get input
+
+using std::string;
+using std::vector;
+
+class House{
+    private:
+    
+        
+    public:
+};
+
+class Member{};
+
+class Registered_Member : public Member{};
+
+class Non_Registered_Member : public Member{};
