98caf035d62b9fb708e3bf8bd59841287ae9bb22
KruskDerTank <thachanhbo@gmail.com>
2022-05-09 16:57:54 +0700

    something

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream> 
 
 int main() {
+
     std::cout << "Hello world" << std::endl;
     std::cout << "Hello Tu" << std::endl;
-
+    
     std::cout << "Something something bla bla" << std::endl;
+
 }
