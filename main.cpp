87956e7b9b74f7fee72e6fd14d609edd4b5dc7ac
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-09 16:34:43 +0700

    Added Welcome Message function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,52 @@
-#include <iostream> 
+#include <iostream>
+
+//* Constants
+#define APP_HEADER "EEET2482/COSC2082 ASSIGNMENT"
+#define APP_NAME "VACATION HOUSE EXCHANGE APPLICATION"
+
+#define LECTURER_NAME "Mr. Linh Tran"
+#define GROUP_NAME "Gucci Gang"
+
+#define AUTHOR1_INFO "s3926761, Vo Hoang Phuc"
+#define AUTHOR2_INFO "s3915195, Le Cam Tu"
+#define AUTHOR3_INFO "s3926793, Nguyen Pham Anh Thu"
+#define AUTHOR4_INFO "s3927460, Nguyen Ngoc Luong"
+
+#define DIVIDER "----------------------------------------------------"
+
+//* Helper preprocessor macros
+// Miscellaneous macros
+#define newl '\n'
+#define new2l '\n\n'
+
+// I/O macros
+#define illog(x) std::cout << x
+#define log(x) std::cout << x << newl
+#define debug(x) std::cout << #x << ": " << x << newl
+#define show_error(x) std::cerr << x << newl
+#define input(x) std::cin >> x
+
+void displayWelcomeMsg() {
+    log(newl << DIVIDER << newl);
+    log(APP_HEADER);
+    log(APP_NAME);
+
+    log(newl << DIVIDER << newl);
+    log("Instructor: " << LECTURER_NAME);
+    log("Group: " << GROUP_NAME);
+
+    log(newl << DIVIDER);
+    log("\t\t\tAuthors");
+    log(DIVIDER << newl);
+
+    log(AUTHOR1_INFO);
+    log(AUTHOR2_INFO);
+    log(AUTHOR3_INFO);
+    log(AUTHOR4_INFO << newl);
+}
 
 int main() {
-    std::cout << "Hello world" << std::endl;
-    std::cout << "Hello Tu" << std::endl;
+    displayWelcomeMsg();
 
-    std::cout << "Something something bla bla" << std::endl;
+    return 0;
 }
