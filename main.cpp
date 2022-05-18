3098e3e5fd547e6f8a34dde544070b5d005384ee
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-05-18 23:56:04 +0700

    Updated main file

--- main.cpp
+++ main.cpp
@@ -14,18 +14,32 @@
 using namespace HouseExchanger;
 
 int main() {
+    //* Setup
     // Create a new system object to manage all the data
     System *system = new System();
 
-    // Load all the data before the user can interact with the system
+    // By default, the system is not initialized.
+    // Therefore, Load all the data before the user can interact with the system.
     bool loadSuccess = system->loadAll();
     if (!loadSuccess)
         return 1;
 
-    // Display welcome message
+    // System is now initialized.
+    // Display welcome message.
     displayWelcomeMsg();
 
-    // Save all the data after the user exits the system
+    // By default, the user is not logged in.
+    // Therefore, Create guest object to interact with the system.
+    Guest *guest = new Guest();
+
+    // To keep track of user data before and after login,
+    // A Member object is created to store the data.
+    Member *member = new Member();
+
+    //* Main loop
+    // The main loop is a while loop that runs until the user quits.
+
+    // Save all the data after the user exits the system.
     bool saveSuccess = system->saveAll();
     if (!saveSuccess)
         return 2;
